c65728612dff598c57c0f7b8b4b873b4235c2e30
pedroaguia8 <24630163+pedroaguia8@users.noreply.github.com>
2025-11-02 22:46:38 +0000

    Test cleanInput function

---
+++ repl_test.go
@@ -0,0 +1,45 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "Charmander Bulbasaur PIKACHU",
+			expected: []string{"charmander", "bulbasaur", "pikachu"},
+		},
+		{
+			input:    "single",
+			expected: []string{"single"},
+		},
+		{
+			input:    "",
+			expected: []string{},
+		},
+		{
+			input:    "   ",
+			expected: []string{},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("expected size: %d, actual: %d", len(c.expected), len(actual))
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("expected: %s, actual: %s", expectedWord, word)
+			}
+		}
+	}
+}
